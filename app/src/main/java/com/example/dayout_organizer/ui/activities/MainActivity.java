a83cd2bbbdc7fc626eaef91c083b4af2c3a8d748
EYAD <eyadan.900@gmail.com>
2022-04-27 05:26:16 +0300

    fix running project issue

--- app/src/main/java/com/example/dayout_organizer/ui/activities/MainActivity.java
+++ app/src/main/java/com/example/dayout_organizer/ui/activities/MainActivity.java
@@ -1,9 +1,11 @@
-package com.example.dayout_organizer;
+package com.example.dayout_organizer.ui.activities;
 
 import android.os.Bundle;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.dayout_organizer.R;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -15,7 +17,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         ButterKnife.bind(this);
     }
 }
