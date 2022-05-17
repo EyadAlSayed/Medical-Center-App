e0e537e3ffa5b3c356b4dae985513edcb26ec411
Caesar <caesarfarah@yahoo.com>
2022-05-17 09:44:50 +0300

    minor fixes.

--- app/src/main/java/com/example/dayout_organizer/models/trip/TripModel.java
+++ app/src/main/java/com/example/dayout_organizer/models/trip/TripModel.java
@@ -12,4 +12,15 @@ public class TripModel {
     public boolean isActive;
     public String state;
     public String date;
+
+    public TripModel(String title, String description, int passengers_count, List<String> stops, List<String> photos, boolean isActive, String state, String date) {
+        this.title = title;
+        this.description = description;
+        this.passengers_count = passengers_count;
+        this.stops = stops;
+        this.photos = photos;
+        this.isActive = isActive;
+        this.state = state;
+        this.date = date;
+    }
 }
