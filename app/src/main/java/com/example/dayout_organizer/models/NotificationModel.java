26c0c2d5b33c8da3d2f69d8fcd2414985ef6b4f2
Caesar <caesarfarah@yahoo.com>
2022-05-18 18:04:55 +0300

    NotificationFragment Design.

---
+++ app/src/main/java/com/example/dayout_organizer/models/NotificationModel.java
@@ -0,0 +1,12 @@
+package com.example.dayout_organizer.models;
+
+public class NotificationModel {
+
+    public String title;
+    public String description;
+
+    public NotificationModel(String title, String description) {
+        this.title = title;
+        this.description = description;
+    }
+}
