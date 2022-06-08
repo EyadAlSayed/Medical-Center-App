c2534f87efb2d3eaf5ee35b612acd097722fdeb7
Caesar <caesarfarah@yahoo.com>
2022-06-08 20:43:56 +0300

    polls and votes + with API - 90% (no API)

---
+++ app/src/main/java/com/example/dayout_organizer/models/poll/VoteData.java
@@ -0,0 +1,12 @@
+package com.example.dayout_organizer.models.poll;
+
+public class VoteData {
+
+    public String title;
+    public int peopleVotes;
+
+    public VoteData(String title, int peopleVotes){
+        this.title = title;
+        this.peopleVotes = peopleVotes;
+    }
+}
